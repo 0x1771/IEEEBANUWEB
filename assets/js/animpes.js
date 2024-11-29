3b4777d2e29a119c2d766bc70dd5f7e092af2bdb
0x1771 <fakesmileux@gmail.com>
2024-11-29 18:32:57 +0200

    burger menu eklendi düzeltmeler yapılacak

---
+++ assets/js/animpes.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    const button = document.querySelector('.Btn-Container');
+    
+    setTimeout(function() {
+      button.classList.add('show');
+    }, 500); 
+  });
+  
